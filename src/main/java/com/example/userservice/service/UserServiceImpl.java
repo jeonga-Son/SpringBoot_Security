bebf038c364e40308a85c2172c8318752274a1fd
“jeonga-Son” <jjeong0106@gmail.com>
2023-03-23 10:09:18 +0900

    [chore] add modelmapper library

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.userservice.jpa.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.UUID;
+
 public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository userRepository;
@@ -15,6 +17,8 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
+
         return null;
     }
 
